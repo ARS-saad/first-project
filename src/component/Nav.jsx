c3416849cea982f051e51570b2bb3f00f47607ff
ARS-saad <abdurrahmansaad46@gmail.com>
2023-09-30 15:14:33 +0600

    I add footer and this project is finished

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,33 +1,33 @@
-import { hamburger } from '../assets/icons'
+// import { hamburger } from '../assets/icons'
 import { headerLogo } from '../assets/images'
 import { navLinks } from '../constants/index'
 
 const Nav = () => {
   return (
     <header className='padding-x py-8 fixed z-10 w-full'>
-        <nav className='flex justify-between items-center max-container '>
+        <nav className='flex justify-between items-center max-container max-sm:h-5'>
             <a href="">
-                <img src={headerLogo} alt="Logo" width={130} height={29} />
+                <img src={headerLogo} alt="Logo" width={130} height={29} className='max-sm:h-5' />
             </a>
-            <ul className='flex-1 flex justify-center items-center gap-16 max-md:hidden'>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-sm:gap-10'>
                 {navLinks.map((item) => (
                     <li key={item.label}>
                         <a 
                           href={item.href} 
-                          className="font-montserrat leading-normal text-slate-gray">
+                          className="font-montserrat leading-normal text-slate-gray max-sm:text-[12px]">
                             {item.label}
                         </a>
                     </li>
                 ))}
             </ul>
-            <div className='hidden max-md:block'>
+            {/* <div className='hidden max-md:block'>
                 <img 
                   src={hamburger}
                   alt='hamburger'
                   width={25}
                   height={25}
                 />
-            </div>
+            </div> */}
         </nav>
     </header>
   )
